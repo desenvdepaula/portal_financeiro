35c2c2a136aa8bf3803d6ed4c0bb9d2362f4caf9
Willian Thiago <willianthiagofozz@hotmail.com>
2025-02-06 14:13:49 -0300

    update recebimento empresarial

--- recibimentos_empresarial/controller.py
+++ recibimentos_empresarial/controller.py
@@ -1,23 +1,19 @@
 from datetime import datetime
-from Database.models import Connection, SQLServerConnection
+from Database.models import PostgreSQLConnection, SQLServerConnection
 from .src.sql import get_recebimentos, get_max_lancamento, get_lancamentos, get_juros
 from .src.dict import get_dicionario, get_filial
 
-class Controller:
+class Controller(PostgreSQLConnection):
     
-    def __init__(self, init_firebid=False, init_sqlServer=False, *args, **kwargs):
-        if init_firebid:
-            init_args = {'host': '192.168.1.14', 'database': '/home/firebird/questor.fdb', 'user': 'sysdba', 'password': 'masterkey'}
-            self.firebird = Connection(**init_args)
-        if init_sqlServer:
-            init_args = {'driver': '{ODBC Driver 17 for SQL Server}', 'server': '192.168.1.28,391', 'database': 'nGestaao3', 'uid': 'sa', 'pwd': 'QER159357XT$'}
-            self.sqlserver = SQLServerConnection(**init_args)
+    def __init__(self, *args, **kwargs):
+        self.default_connect()
+        init_args = {'driver': '{ODBC Driver 17 for SQL Server}', 'server': '192.168.1.28,391', 'database': 'nGestaao3', 'uid': 'sa', 'pwd': 'QER159357XT$'}
+        self.sqlserver = SQLServerConnection(**init_args)
 
     def gerarRecebimentosJuros(self, inicio_periodo, fim_periodo, relacao_empresas):
+        self.connect()
+        self.sqlserver.connect()
         try:
-            self.firebird.connect()
-            self.sqlserver.connect()
-
             self.sqlserver.execute_sql("SET IDENTITY_INSERT TBL_FINANCEIRO_CONTA_CORRENTE_LANCAMENTOS_DIARIOS ON")
             self.sqlserver.commit_changes()
 
@@ -42,7 +38,7 @@ class Controller:
                     conferencia.append(i)
 
                 cnpj = dicescritorio[escritorio][0]
-                query = self.firebird.execute_sql(get_juros(escritorio,dataini,datafim,codigos))
+                query = self.run_query_for_select(get_juros(escritorio,dataini,datafim,codigos))
                 array = []
 
                 for i in query:
@@ -62,18 +58,8 @@ class Controller:
                         obs = i[4].replace("'",'')
                         if [int(numero),data[:-4]]  not in [[int(r[0]),str(r[1])] for r in conferencia]: 
                             insert = f"""   
-                                INSERT INTO nGestaao3.dbo.TBL_FINANCEIRO_CONTA_CORRENTE_LANCAMENTOS_DIARIOS
-                                (CD_LANCAMENTO,CD_ENTIDADE,CD_CONTA_ENTRADA,CD_CONTA_SAIDA,CD_EMPRESA,CD_FILIAL,CD_CONTA_CORRENTE,
-                                CD_PDV,CD_USUARIO,CD_USUARIOAT,CD_HISTORICO,DT_LANCAMENTO,DT_CONTABIL,DT_CADASTRO,DT_ATUALIZACAO,
-                                DS_REFERENTE,DS_OBS,NR_DOCUMENTO,NR_CHEQUE,VL_ENTRADA,VL_SAIDA,DT_CONCILIACAO,DT_CONCILIACAO_SERVER,
-                                CD_USUARIO_CONCILIACAO,CD_NOTA_FATURAMENTO,CD_ENTRADA_COMPRA,CD_CONTA_GERENCIAL,CD_CHAVE_QUESTOR_TRIBUTARIO,
-                                CD_STATUS_INTEGRACAO_QUESTOR,CD_CONHECIMENTO,DS_ESPECIE,CD_LANCAMENTO_STANDALONE,CD_MOEDA,CD_NOTA,
-                                CD_PDV_STANDALONE,NR_SERIE_STANDALONE,NR_SERIE_ECF,CD_ORIGEM_DADOS,CD_LANCAMENTO_ORIGEM,CD_ORDEM_SERVICO,
-                                DS_MD5_DT_LANCAMENTO,DS_MD5_VL_LANCAMENTO,CD_TABELA_CONTABIL,VL_DESCONTO,CD_CONVERSAO,CD_CAIXA)
-                                VALUES({maximoLancamento[0] + 1},{entidade},2860,NULL,1,{filial},{corrente},NULL,0,0,299,'{dataFormat}','{dataFormat}',
-                                CAST(GETDATE() AS DATETIME),CAST(GETDATE() AS DATETIME),'{obs[:100]}','{obs}','{numero}',NULL,{i[5]},
-                                0.00,NULL,NULL,NULL,NULL,NULL,NULL,NULL,1,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,
-                                NULL,NULL,NULL,NULL,NULL,NULL);"""
+                                INSERT INTO nGestaao3.dbo.TBL_FINANCEIRO_CONTA_CORRENTE_LANCAMENTOS_DIARIOS(CD_LANCAMENTO,CD_ENTIDADE,CD_CONTA_ENTRADA,CD_CONTA_SAIDA,CD_EMPRESA,CD_FILIAL,CD_CONTA_CORRENTE,CD_PDV,CD_USUARIO,CD_USUARIOAT,CD_HISTORICO,DT_LANCAMENTO,DT_CONTABIL,DT_CADASTRO,DT_ATUALIZACAO,DS_REFERENTE,DS_OBS,NR_DOCUMENTO,NR_CHEQUE,VL_ENTRADA,VL_SAIDA,DT_CONCILIACAO,DT_CONCILIACAO_SERVER,CD_USUARIO_CONCILIACAO,CD_NOTA_FATURAMENTO,CD_ENTRADA_COMPRA,CD_CONTA_GERENCIAL,CD_CHAVE_QUESTOR_TRIBUTARIO,CD_STATUS_INTEGRACAO_QUESTOR,CD_CONHECIMENTO,DS_ESPECIE,CD_LANCAMENTO_STANDALONE,CD_MOEDA,CD_NOTA,CD_PDV_STANDALONE,NR_SERIE_STANDALONE,NR_SERIE_ECF,CD_ORIGEM_DADOS,CD_LANCAMENTO_ORIGEM,CD_ORDEM_SERVICO,DS_MD5_DT_LANCAMENTO,DS_MD5_VL_LANCAMENTO,CD_TABELA_CONTABIL,VL_DESCONTO,CD_CONVERSAO,CD_CAIXA)VALUES({maximoLancamento[0] + 1},{entidade},2860,NULL,1,{filial},{corrente},NULL,0,0,299,'{dataFormat}','{dataFormat}',CAST(GETDATE() AS DATETIME),CAST(GETDATE() AS DATETIME),'{obs[:100]}','{obs}','{numero}',NULL,{i[5]},0.00,NULL,NULL,NULL,NULL,NULL,NULL,NULL,1,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+                            """
 
                             self.sqlserver.execute_sql(insert)
                             self.sqlserver.commit_changes()
@@ -82,18 +68,16 @@ class Controller:
             
         except Exception as error:
             raise Exception(error)
-        else:
+        finally:
+            self.disconnect()
             self.sqlserver.execute_sql("SET IDENTITY_INSERT TBL_FINANCEIRO_CONTA_CORRENTE_LANCAMENTOS_DIARIOS OFF")
             self.sqlserver.commit_changes()
-        finally:
-            self.firebird.disconnect()
             self.sqlserver.disconnect()
 
     def gerarRecebimentosRecebimentos(self, inicio_periodo, fim_periodo, relacao_empresas):
+        self.connect()
+        self.sqlserver.connect()
         try:
-            self.firebird.connect()
-            self.sqlserver.connect()
-
             self.sqlserver.execute_sql("SET IDENTITY_INSERT TBL_FINANCEIRO_CONTA_CORRENTE_LANCAMENTOS_DIARIOS ON")
             self.sqlserver.commit_changes()
 
@@ -118,7 +102,7 @@ class Controller:
                     conferencia.append(i)
 
                 cnpj = dicescritorio[escritorio][0]
-                query = self.firebird.execute_sql(get_recebimentos(escritorio,dataini,datafim,codigos))
+                query = self.run_query_for_select(get_recebimentos(escritorio,dataini,datafim,codigos))
                 array = []
 
                 for i in query:
@@ -138,18 +122,8 @@ class Controller:
                         obs = i[4].replace("'",'')
                         if [int(numero),data[:-4]]  not in [[int(r[0]),str(r[1])] for r in conferencia]:
                             insert = f"""   
-                            INSERT INTO nGestaao3.dbo.TBL_FINANCEIRO_CONTA_CORRENTE_LANCAMENTOS_DIARIOS
-                            (CD_LANCAMENTO,CD_ENTIDADE,CD_CONTA_ENTRADA,CD_CONTA_SAIDA,CD_EMPRESA,CD_FILIAL,CD_CONTA_CORRENTE,
-                            CD_PDV,CD_USUARIO,CD_USUARIOAT,CD_HISTORICO,DT_LANCAMENTO,DT_CONTABIL,DT_CADASTRO,DT_ATUALIZACAO,
-                            DS_REFERENTE,DS_OBS,NR_DOCUMENTO,NR_CHEQUE,VL_ENTRADA,VL_SAIDA,DT_CONCILIACAO,DT_CONCILIACAO_SERVER,
-                            CD_USUARIO_CONCILIACAO,CD_NOTA_FATURAMENTO,CD_ENTRADA_COMPRA,CD_CONTA_GERENCIAL,CD_CHAVE_QUESTOR_TRIBUTARIO,
-                            CD_STATUS_INTEGRACAO_QUESTOR,CD_CONHECIMENTO,DS_ESPECIE,CD_LANCAMENTO_STANDALONE,CD_MOEDA,CD_NOTA,
-                            CD_PDV_STANDALONE,NR_SERIE_STANDALONE,NR_SERIE_ECF,CD_ORIGEM_DADOS,CD_LANCAMENTO_ORIGEM,CD_ORDEM_SERVICO,
-                            DS_MD5_DT_LANCAMENTO,DS_MD5_VL_LANCAMENTO,CD_TABELA_CONTABIL,VL_DESCONTO,CD_CONVERSAO,CD_CAIXA)
-                            VALUES({maximoLancamento[0] + 1},{entidade},{contabil},NULL,1,{filial},{corrente},NULL,0,0,{historico},'{dataFormat}','{dataFormat}',
-                            CAST(GETDATE() AS DATETIME),CAST(GETDATE() AS DATETIME),'{obs[:100]}','{obs}','{numero}',NULL,{i[5]},
-                            0.00,NULL,NULL,NULL,NULL,NULL,NULL,NULL,1,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,
-                            NULL,NULL,NULL,NULL,NULL,NULL);"""
+                                INSERT INTO nGestaao3.dbo.TBL_FINANCEIRO_CONTA_CORRENTE_LANCAMENTOS_DIARIOS(CD_LANCAMENTO,CD_ENTIDADE,CD_CONTA_ENTRADA,CD_CONTA_SAIDA,CD_EMPRESA,CD_FILIAL,CD_CONTA_CORRENTE,CD_PDV,CD_USUARIO,CD_USUARIOAT,CD_HISTORICO,DT_LANCAMENTO,DT_CONTABIL,DT_CADASTRO,DT_ATUALIZACAO,DS_REFERENTE,DS_OBS,NR_DOCUMENTO,NR_CHEQUE,VL_ENTRADA,VL_SAIDA,DT_CONCILIACAO,DT_CONCILIACAO_SERVER,CD_USUARIO_CONCILIACAO,CD_NOTA_FATURAMENTO,CD_ENTRADA_COMPRA,CD_CONTA_GERENCIAL,CD_CHAVE_QUESTOR_TRIBUTARIO,CD_STATUS_INTEGRACAO_QUESTOR,CD_CONHECIMENTO,DS_ESPECIE,CD_LANCAMENTO_STANDALONE,CD_MOEDA,CD_NOTA,CD_PDV_STANDALONE,NR_SERIE_STANDALONE,NR_SERIE_ECF,CD_ORIGEM_DADOS,CD_LANCAMENTO_ORIGEM,CD_ORDEM_SERVICO,DS_MD5_DT_LANCAMENTO,DS_MD5_VL_LANCAMENTO,CD_TABELA_CONTABIL,VL_DESCONTO,CD_CONVERSAO,CD_CAIXA)VALUES({maximoLancamento[0] + 1},{entidade},{contabil},NULL,1,{filial},{corrente},NULL,0,0,{historico},'{dataFormat}','{dataFormat}',CAST(GETDATE() AS DATETIME),CAST(GETDATE() AS DATETIME),'{obs[:100]}','{obs}','{numero}',NULL,{i[5]},0.00,NULL,NULL,NULL,NULL,NULL,NULL,NULL,1,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+                            """
 
                             self.sqlserver.execute_sql(insert)
                             self.sqlserver.commit_changes()
@@ -158,9 +132,8 @@ class Controller:
             
         except Exception as error:
             raise Exception(error)
-        else:
+        finally:
+            self.disconnect()
             self.sqlserver.execute_sql("SET IDENTITY_INSERT TBL_FINANCEIRO_CONTA_CORRENTE_LANCAMENTOS_DIARIOS OFF")
             self.sqlserver.commit_changes()
-        finally:
-            self.firebird.disconnect()
             self.sqlserver.disconnect()
--- recibimentos_empresarial/src/sql.py
+++ recibimentos_empresarial/src/sql.py
@@ -1,45 +1,46 @@
 def get_recebimentos(escritorio,dataini,datafim,codigos):
     codigos = codigos if len(codigos) > 1 else f"({codigos[0]})"
     sql = """
-        SELECT
-            C.CODIGOESCRIT,
-            C.DATARCTOCR,
-            C.CODIGOCAIXACONTA, 
-            C.NUMERODCTOCR,
-	        (R.CODIGOCLIENTE||' - '||P.NOME) CLIENTE,
-            SUM(C.VALORRCTOCR) VALOR
-        FROM
-            CONTARECEBIDA C
-        JOIN
-            CONTARECEBER R ON
-            C.CODIGOESCRIT = R.CODIGOESCRIT AND
-            C.NUMERODCTOCR = R.NUMERODCTOCR 
-        JOIN 
-            PESSOAFINANCEIRO P ON
-            R.CODIGOCLIENTE = P.CODIGOPESSOAFIN 
-        WHERE
-            C.CODIGOESCRIT = {0} AND
-            C.CODIGOCAIXACONTA IN {3} AND
-            C.DATARCTOCR BETWEEN '{1}' AND '{2}' 
-        GROUP BY 1,2,3,4,5
-        UNION ALL
-        SELECT 
-            S.CODIGOESCRIT,
-            S.DATAEMISSAONS,
-            S.CODIGOCAIXACONTA,
-	        S.SERIENS ||S.NUMERONS NUMERODCTOCR,
-	        (S.CODIGOCLIENTE||' - '||P.NOME) CLIENTE,
-            SUM(S.VALORBRUTO) VALOR
-        FROM
-            SERVICONOTA S
-        JOIN 
-            PESSOAFINANCEIRO P ON
-            S.CODIGOCLIENTE = P.CODIGOPESSOAFIN 
-        WHERE
-            S.CODIGOESCRIT = {0} AND
-            CODIGOCAIXACONTA IN {3} AND
-            S.DATAEMISSAONS BETWEEN '{1}' AND '{2}' 
-        GROUP BY 1,2,3,4,5
+        select
+            c.codigoescrit,
+            c.datarctocr,
+            c.codigocaixaconta, 
+            c.numerodctocr,
+	        (r.codigocliente||' - '||p.nome) cliente,
+            sum(c.valorrctocr) valor
+        from
+            contarecebida c
+        join
+            contareceber r on
+            c.codigoescrit = r.codigoescrit and
+            c.numerodctocr = r.numerodctocr 
+        join 
+            pessoafinanceiro p on
+            r.codigocliente = p.codigopessoafin 
+        where
+            c.codigoescrit = {0} and
+            c.codigocaixaconta in {3} and
+            c.datarctocr between '{1}' and '{2}' 
+        group by 1,2,3,4,5
+        union all
+        select 
+            s.codigoescrit,
+            s.dataemissaons,
+            s.codigocaixaconta,
+	        s.seriens ||s.numerons numerodctocr,
+	        (s.codigocliente||' - '||p.nome) cliente,
+            sum(s.valorbruto) valor
+        from
+            serviconota s
+        join 
+            pessoafinanceiro p on
+            s.codigocliente = p.codigopessoafin 
+        where
+            s.codigoescrit = {0} and
+            codigocaixaconta in {3} and
+            s.dataemissaons between '{1}' and '{2}' 
+        group by
+            1,2,3,4,5
     """.format(escritorio,dataini,datafim,codigos)
     return sql
 
@@ -62,28 +63,30 @@ def get_lancamentos(filial,dataini,datafim):
 def get_juros(escritorio,dataini,datafim,codigos):
     codigos = codigos if len(codigos) > 1 else f"({codigos[0]})"
     sql = """
-        SELECT
-            CAST(SUBSTRING(C.CODIGOESCRIT FROM 2 FOR 3) AS NUMERIC) CODIGOESCRIT,
-            C.DATARCTOCR,
-            C.CODIGOCAIXACONTA, 
-            C.NUMERODCTOCR,
-            (R.CODIGOCLIENTE||' - '||P.NOME) CLIENTE,
-            SUM(C.MULTARCTOCR+C.JURORCTOCR) VALOR
-        FROM
-            CONTARECEBIDA C
-        JOIN
-            CONTARECEBER R ON
-            C.CODIGOESCRIT = R.CODIGOESCRIT AND
-            C.NUMERODCTOCR = R.NUMERODCTOCR 
-        JOIN 
-            PESSOAFINANCEIRO P ON
-            R.CODIGOCLIENTE = P.CODIGOPESSOAFIN 
-        WHERE
-            C.CODIGOESCRIT = 9000+{0} AND
-            C.CODIGOCAIXACONTA IN {3} AND
-            C.DATARCTOCR BETWEEN '{1}' AND '{2}' 
-        GROUP BY 1,2,3,4,5
-        HAVING SUM(C.MULTARCTOCR+C.JURORCTOCR) <> 0
+        select
+            cast(substring(c.codigoescrit::text from 2 for 3) as numeric) codigoescrit,
+            c.datarctocr,
+            c.codigocaixaconta, 
+            c.numerodctocr,
+            (r.codigocliente||' - '||p.nome) cliente,
+            sum(c.multarctocr+c.jurorctocr) valor
+        from
+            contarecebida c
+        join
+            contareceber r on
+            c.codigoescrit = r.codigoescrit and
+            c.numerodctocr = r.numerodctocr 
+        join 
+            pessoafinanceiro p on
+            r.codigocliente = p.codigopessoafin 
+        where
+            c.codigoescrit = 9000+{0} and
+            c.codigocaixaconta in {3} and
+            c.datarctocr between '{1}' and '{2}' 
+        group by
+            1,2,3,4,5
+        having
+            sum(c.multarctocr+c.jurorctocr) <> 0
     """.format(escritorio,dataini,datafim,codigos)
     return sql
 
