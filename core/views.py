ede87fd863089f54ea7a1dadee10e4b78099f426
Willian Thiago <willianthiagofozz@hotmail.com>
2025-12-13 09:00:55 -0300

    trocando o timbrado

--- core/views.py
+++ core/views.py
@@ -23,6 +23,24 @@ def erro404(request):
     template = "erro404.html"
     return render(request, template)
 
+def get_request_to_api_omie(escritorio, call, params):
+    empresas = {
+        '501': {'app_key': "829724170275", 'app_secret': "a1d06fbf0ae8801964e0d56346527aa9"},   # DE PAULA CONTADORES
+        '502': {'app_key': "6645667020993", 'app_secret': "cb13871cfbbd6b401ceeb3a14abc4c50"},   # DE PAULA SOLUCOES CONTABEIS LTDA
+        '505': {'app_key': "6646111687215", 'app_secret': "e54bf4e6e255a1a4a2c0a2dd844df485"},   # AR DE PAULA FOZ
+        '567': {'app_key': "6646134687192", 'app_secret': "1a78910abf26b0a1787fb0d119a1cdb7"},   # DE PAULA SOLUCOES EMPRESARIAIS LTDA
+        '575': {'app_key': "6646142353851", 'app_secret': "60ddd49e3c42214711cbe016ee8aae82"},   # DPC SOLUCOES LTDA
+    }
+    base_request = {
+        "call": call,
+        "app_key": empresas[escritorio]['app_key'],
+        "app_secret": empresas[escritorio]['app_secret'], 
+        "param": [
+            params
+        ]
+    }
+    return base_request
+
 def request_project_log(cod_empresa, formData, aplication, username):
     # ---> Escrever na Tabela do Banco de Dados
     data_hoje = dt.now().strftime('%d/%m/%Y %H:%M:%S')
